209d198f2726fddf8ed0e04713699655e6a20b00
iribeirodev <ribeirojr.itamar@gmail.com>
2021-11-03 14:36:42 -0300

    User validation attribute

--- Request/CreateUserRequest.cs
+++ Request/CreateUserRequest.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using ShopAutenticacao.Models.Validation;
+
 namespace ShopAutenticacao.Request
 {
     public class CreateUserRequest
     {
+        [StringLength(12, MinimumLength = 5,
+        ErrorMessage = "Username deve ter entre 10 e 15 caracteres")]
         public string Username { get; set; }
+
+        [StringLength(12, MinimumLength = 5,
+        ErrorMessage = "Password deve ter entre 12 e 36 caracteres")]
         public string Password { get; set; }
+
+        [Required]
+        [RoleAttribute]
         public string Role { get; set; }
     }
 }
